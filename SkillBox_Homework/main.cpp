10559862efa710686ef0a6bbc3d24249471693af
Tony_Stark817 <stark.industries817@gmail.com>
2022-10-09 14:02:38 +0300

    Homework 11.4 completed.

--- SkillBox_Homework/main.cpp
+++ SkillBox_Homework/main.cpp
@@ -1,36 +1,52 @@
-//
-//  main.cpp
-//  SkillBox_Homework 18.5
-//
-//  Created by Tony Stark817 on 09.10.2022.
-//
+/*
+  main.cpp
+ 
+  SkillBox_Homework 11.4
+
+  Created by Tony Stark817(Nikolay Gridin) on 09.10.2022.
+*/
 
 #include <iostream>
 
+
 using namespace std;
 
-int main() {
-  int steck[20];
-  int i = -1;  // объявили стек
+// Вызываем функцию std::cout
+void print()
+{
+    // Потому что хотим использовать стандартную библиотеку для печати в консоль и
+    // не использовать какие либо другие библиотеки.
+    cout << "Hello SkillBox!\n";
     
-  for (int j = 0; j < 3; j++) {
-    int a;
-       
-    cin >> a;
-
-    i++;  // увеличиваем i на один
-      
-    steck[i] = a;  // добавляем в стек элемент
-  }
-   
-  if (i == -1) cout << "Стек пуст";  // проверяем пуст ли стек (нет)
-    
-  cout << steck[i] << " это верхний элемент стека";
-    
-  cout << "Сейчас мы удалим верхний элемент";
-    
-  i--;  // уменьшаем i на один
-
-  system("pause");
-  return 0;
 }
+/*
+  Присваиваем значение переменным и печатаем Hello World!
+ */
+int main()
+{
+    int x = 100;
+    3 + 7;
+    int y = x + 100;
+    
+    int b = 0;
+    b = b + 2;
+    
+    int test;
+    test = 100500;
+    
+    int test2 = 1005001;
+    
+    
+    
+    int mult = x * y;
+    int random = 1002;
+    
+    cout << random << "\n";
+    
+    
+    
+}
+
+
+ 
+
