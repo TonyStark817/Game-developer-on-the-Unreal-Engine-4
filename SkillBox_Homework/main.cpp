2e80101c7c1d8ec48629a5a635841212c040e91f
Tony_Stark817 <stark.industries817@gmail.com>
2022-10-09 20:05:02 +0300

    Homework 13.4 completed.

--- SkillBox_Homework/main.cpp
+++ SkillBox_Homework/main.cpp
@@ -1,49 +1,23 @@
 /*
   main.cpp
  
-  SkillBox_Homework 11.4
+  SkillBox_Homework 13.4
 
   Created by Tony Stark817(Nikolay Gridin) on 09.10.2022.
 */
 
 #include <iostream>
+#include "Helpers.h"
+
 
 
 using namespace std;
 
-// Вызываем функцию std::cout
-void print()
-{
-    // Потому что хотим использовать стандартную библиотеку для печати в консоль и
-    // не использовать какие либо другие библиотеки.
-    cout << "Hello SkillBox!\n";
-    
-}
-/*
-  Присваиваем значение переменным и печатаем Hello World!
- */
 int main()
 {
-    int x = 100;
-    3 + 7;
-    int y = x + 100;
+    cout << Kvadrat(12, 36) << endl;
     
-    int b = 0;
-    b = b + 2;
-    
-    int test;
-    test = 100500;
-    
-    int test2 = 1005001;
-    
-    
-    
-    int mult = x * y;
-    int random = 1002;
-    
-    cout << random << "\n";
-    
-    
+    return 0;
     
 }
 
