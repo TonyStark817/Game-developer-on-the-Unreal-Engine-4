e3469e1af0d0505e6d9a45cd164c1b6a5fbe8cd8
Tony_Stark817 <stark.industries817@gmail.com>
2022-10-10 00:05:19 +0300

    Homework 14.4 completed.

--- SkillBox_Homework/main.cpp
+++ SkillBox_Homework/main.cpp
@@ -1,21 +1,24 @@
 /*
   main.cpp
  
-  SkillBox_Homework 13.4
+  SkillBox_Homework 14.4
 
   Created by Tony Stark817(Nikolay Gridin) on 09.10.2022.
 */
 
 #include <iostream>
-#include "Helpers.h"
-
-
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << Kvadrat(12, 36) << endl;
+    string Skill = "SkillBox";
+    
+    cout << "Значение переменной = " << Skill << "\n";
+    cout << "Количество символов = " << Skill.length() << "\n";
+    cout << "Первый символ = " << Skill [0] << "\n";
+    cout << "Последний символ = " << Skill [7]<< "\n";
     
     return 0;
     
