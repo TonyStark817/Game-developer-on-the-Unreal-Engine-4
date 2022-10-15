b4c17a3104a902bf098c2762a61f561d11bdd70b
Tony_Stark817 <stark.industries817@gmail.com>
2022-10-15 17:18:02 +0300

    Homework 17.5 completed.
    Completed the practical work 17.5

--- SkillBox_Homework/main.cpp
+++ SkillBox_Homework/main.cpp
@@ -3,55 +3,50 @@
  
   Моя учёба в SkillBox на курсе "Разработчик игр на Unreal Engine 4" (C++)
  
-  SkillBox_Homework 16.5
+  SkillBox_Homework 17.5
 
   Created by Tony Stark817(Nikolay Gridin) on 09.10.2022.
 */
 
 #include <iostream>
-#include <ctime>
-#include <time.h>
+#include <cstdio>
+#include <cmath>
 
 using namespace std;
 
+class Vector
+{
 
+private:
+
+    double x = 9;
+    double y = 9;
+    double z = 9;
+  
+
+public:
+
+    Vector() : x(10), y(10), z(10)
+    {}
+    Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z)
+    {}
+
+
+    void Show()
+    {
+        cout << '\n' << x << ' ' << y << ' ' << z;
+    }
+
+    double Lenght()
+    {
+        return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+    }
+};
 
 int main()
 {
-    
-    const int N = 5;
-    int array[N][N];
-    int sum = 0; /*переменная для хранения суммы чисел*/
- 
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            array[i][j] = i + j;
-        }
-    }
- 
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-         cout << array[i][j] << " ";
-        
-        }
-        cout << endl;
-    }
-    
-    struct tm newtime;
-    time_t t_now = time(0);
-     int Day = localtime(&t_now)->tm_mday % N;
-    cout << "День = " << Day << "\n";
-
-    for (int j = 0; j < N; j++)
-    {
-           sum = sum + array[Day][j]; /*проходимся по строке и складываем числа в ней*/
-    }
-    
-    cout << "Cумму элементов в строке массива = " << sum << endl;
-
-    return 0;
+    Vector v;
+    v.Show();
+    double MyLenght = v.Lenght();
+    cout << '\n' << "Длина (модуль) вектора = " << v.Lenght() << endl;
 }
